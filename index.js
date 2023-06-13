9e39095de17bb682bd7dadbca6474df4d8a1b8ff
MikeZuck1 <105445747+MikeZuck1@users.noreply.github.com>
2023-06-13 09:30:40 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Import stylesheets
+import './style.css';
+
+const appDiv = document.getElementById('app');
+appDiv.innerHTML;
+
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+for (let i = 0; i < vacationSpots.length; i++) {
+  console.log('I would love to visit ' + vacationSpots[i]); 
+};  
+
+
